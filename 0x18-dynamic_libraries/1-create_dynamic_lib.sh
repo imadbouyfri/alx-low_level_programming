987b054441c0f1aa119176e6aa80c2b76986505e
imad bouyfri <imad.bouyfri@outlook.fr>
2023-08-22 00:47:22 -0700

    create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-gcc -fPIC -c *.c
-gcc -shared -o liball.so *.o
 
+# List of selected .c files
+selected_files=$(ls | grep '^[^0-9].*\.c$')
+
+# Compile selected .c files into object files
+gcc -Wall -Werror -Wextra -pedantic -c $selected_files
+
+# Create the dynamic library
+gcc -shared -o liball.so $(ls | grep '^[^0-9].*\.o$')
+
+
+# Print a success message
+echo "Dynamic library liball.so created successfully!"
